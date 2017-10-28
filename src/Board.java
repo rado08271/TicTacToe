7562bb08c15c782dea8a667d3856373c7b489684
rado08271 <figura.radoslav@gmail.com>
2017-10-29 00:55:56 +0100

    first commit

--- src/Board.java
+++ src/Board.java
@@ -1,5 +1,3 @@
-import com.sun.prism.Graphics;
-import com.sun.prism.paint.Color;
 
 import javax.swing.*;
 import java.awt.*;
@@ -11,24 +9,29 @@ import java.awt.event.MouseListener;
     public class Board extends JFrame implements MouseListener, ActionListener{
 
         Frame frame = new Frame();
+        private int playerTurn = 1;
 
-
-
-
-        public void draw(Graphics g){
-            g.setColor(Color.black);
-            g.drawLine(frame.xAxis/2,frame.yAxes/2,frame.xAxis/2-40,frame.yAxes/2-40);
-
-
+        public boolean whoseTurn(){
+            for(int i=1;i<=9;i++){
+                if((i % 2 == playerTurn )){
+                }
+            }
+            return true;
         }
 
+        public void paint(Graphics g){
+            g.setColor(Color.white);
+            g.drawRect(0,0,frame.xAxis,frame.yAxis);
 
+            g.setColor(Color.black);
+            g.drawRect(frame.xAxis/1/3,0,frame.xAxis/3+30,frame.yAxis);
+            g.drawRect(frame.xAxis/2/3,0,frame.xAxis/2/3+30,frame.yAxis);
+            g.drawRect(0,frame.yAxis/1/3,frame.xAxis,frame.yAxis/1/3);
+            g.drawRect(0,frame.yAxis/2/3,frame.xAxis,frame.yAxis/2/3);
 
+            g.dispose();
 
-
-
-
-
+        }
 
         @Override
         public void actionPerformed(ActionEvent e) {
@@ -39,6 +42,12 @@ import java.awt.event.MouseListener;
         public void mouseClicked(MouseEvent e) {
 
         }
+
+
+
+
+
+
 
         @Override
         public void mousePressed(MouseEvent e) {
--- src/Frame.java
+++ src/Frame.java
@@ -1,23 +1,27 @@
-import javax.swing.*;
+import javax.swing.JFrame;
 import java.awt.*;
 
 public class Frame {
-    public static void main(String[] args){
-        int xAxis = 720;
-        int yAxis = 480;
 
+    public static int xAxis;
+    public static int yAxis;
+
+    public static void main(String[] args){
+        xAxis = 720;
+        yAxis = 480;
 
         JFrame panel = new JFrame();
+        Board board = new Board();
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         int height = (int) screenSize.getHeight();
         int width = (int) screenSize.getWidth();
         panel.setBounds(width/2-360,height/2-240, xAxis,yAxis);
-        panel.setVisible(true);
         panel.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         panel.setResizable(false);
         panel.setTitle("TicTacToe");
 
-
+        panel.add(board);
+        panel.setVisible(true);
 
     }
 
