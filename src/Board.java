ecb40a270cddf693b573b85ce6865fcb2c124a3b
rado08271 <figura.radoslav@gmail.com>
2017-11-15 10:47:56 +0100

    Adding playability to the board

--- src/Board.java
+++ src/Board.java
@@ -1,33 +1,45 @@
 
-import javax.swing.*;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
-    public class Board extends JFrame implements MouseListener, ActionListener{
+    public class Board extends JPanel implements MouseListener, ActionListener{
 
         Frame frame = new Frame();
+        private boolean startGame = false;
         private int playerTurn = 1;
 
-        public boolean whoseTurn(){
-            for(int i=1;i<=9;i++){
-                if((i % 2 == playerTurn )){
-                }
-            }
-            return true;
+        private boolean AIturn = false;
+
+        private Timer time;
+
+        String[][] boardFrame = new String[3][3];
+        //for boardFrame[0][0]
+        //constructor
+        public Board(){
+            addMouseListener(this);
+            setFocusable(true);
+            setFocusTraversalKeysEnabled(false);
+
+            time = new Timer(1,this);
+            time.start();
         }
 
+        //GUI
         public void paint(Graphics g){
             g.setColor(Color.white);
-            g.drawRect(0,0,frame.xAxis,frame.yAxis);
+            g.fillRect(0,0,frame.xAxis,frame.yAxis);
 
             g.setColor(Color.black);
-            g.drawRect(frame.xAxis/1/3,0,frame.xAxis/3+30,frame.yAxis);
-            g.drawRect(frame.xAxis/2/3,0,frame.xAxis/2/3+30,frame.yAxis);
-            g.drawRect(0,frame.yAxis/1/3,frame.xAxis,frame.yAxis/1/3);
-            g.drawRect(0,frame.yAxis/2/3,frame.xAxis,frame.yAxis/2/3);
+            g.fillRect(frame.xAxis*1/3,0,frame.xAxis/200,frame.yAxis);
+            g.fillRect(frame.xAxis*2/3,0,frame.xAxis/200,frame.yAxis);
+            g.fillRect(0,frame.yAxis*1/3-20,frame.xAxis,frame.yAxis/200);
+            g.fillRect(0,frame.yAxis*2/3-20,frame.xAxis,frame.yAxis/200);
 
             g.dispose();
 
@@ -35,7 +47,9 @@ import java.awt.event.MouseListener;
 
         @Override
         public void actionPerformed(ActionEvent e) {
-
+            if(mouseClicked();){
+                getGraphics().fillRect(0,0,frame.xAxis/200,frame.yAxis*2/3);
+            }
         }
 
         @Override
@@ -48,7 +62,7 @@ import java.awt.event.MouseListener;
 
 
 
-
+        //no usage for now
         @Override
         public void mousePressed(MouseEvent e) {
 
